07ac004742a78162cf5d3e0bcdda301c1c3024cc
zondagor <debryniuk@gmail.com>
2020-09-18 08:00:58 +0300

    very very messy negative operations need to be fixed

--- ten/Assignment10Part1.java
+++ ten/Assignment10Part1.java
@@ -10,11 +10,11 @@ import java.util.Arrays;
  * https://stackoverflow.com/questions/7162889/android-java-regex-match-plus-sign
  */
 public class Assignment10Part1 {
-    private static final String[] prioritizedOperations = {"^", "^", "*", "/", "+", "-"};
+    private static final String[] prioritizedOperations = {"^", "*", "/", "+", "-"};
 
     public static void main(String[] args) {
-        if (args.length > 0) calculate(args);
 //        if (args.length > 0) System.out.println(Arrays.toString(args));
+        if (args.length > 0) calculate(args);
         else System.out.println("sorry no arguments found");
     }
 
@@ -30,33 +30,47 @@ public class Assignment10Part1 {
         StringBuilder formula = new StringBuilder(args[0]);
         System.out.println("start " + formula);
 
-//        "2+ (3 + 5) + (1 + 3) ^ 2 "
-        int indexOfParanthesis;
-        while ((indexOfParanthesis = formula.indexOf("(")) > -1) {
-            int indexOfParanthesis2 = formula.indexOf(")");
-            String expression = formula.substring(indexOfParanthesis + 1, indexOfParanthesis2);
-            StringBuilder res = perform(new StringBuilder(expression));
+//        // get rid of pARANTHESIS
+//        int indexOfParanthesis;
+//        while ((indexOfParanthesis = formula.indexOf("(")) > -1) {
+//            int indexOfParanthesis2 = formula.indexOf(")");
+//            String expression = formula.substring(indexOfParanthesis + 1, indexOfParanthesis2);
+//            StringBuilder res = perform(new StringBuilder(expression));
+//
+//            formula.replace(indexOfParanthesis, indexOfParanthesis2 + 1, res.toString());
+//            System.out.println(formula);
+//        }
 
-            formula.replace(indexOfParanthesis, indexOfParanthesis2 + 1, res.toString());
-            System.out.println(formula);
-        }
+        powerUp(formula);
 
-        perform(formula);
+        multiOrDivide(formula);
+
+        addOrSubstract(formula);
+
         System.out.println("finish: " + formula);
     }
 
-    private static StringBuilder perform(StringBuilder formula) {
-        for (int i = 0; i < prioritizedOperations.length; i += 2) {
-            String firstOperation = prioritizedOperations[i];
-            String secondOperation = prioritizedOperations[i + 1];
+    private static void powerUp(StringBuilder formula) {
+        int indOfPower;
+        while ((indOfPower = formula.indexOf("^")) > -1) {
+            int indOperationBefore = findOperationIndex(formula.toString(), indOfPower - 1, false);
+            int nextOperationInd = findOperationIndex(formula.toString(), indOfPower + 1, true);
+            int indOfDigitBeforePower = indOperationBefore + 1;
 
-            doOperations(formula, firstOperation, secondOperation);
+            // if this is the last operation in the formula
+            if (nextOperationInd == -1) nextOperationInd = formula.length();
+
+            String expression = formula.substring(indOfDigitBeforePower, nextOperationInd);
+            double result = doArithmetic(expression, "^");
+
+            formula.replace(indOfDigitBeforePower, nextOperationInd, String.valueOf(result));
+            System.out.println(formula);
         }
-
-        return formula;
     }
 
-    private static void doOperations(StringBuilder formula, String firstOperation, String secondOperation) {
+    private static void multiOrDivide(StringBuilder formula) {
+        String firstOperation = "*";
+        String secondOperation = "/";
         int indOfFirstOperation;
         int indOfSecondOperation;
 
@@ -94,8 +108,99 @@ public class Assignment10Part1 {
         }
     }
 
+    private static void addOrSubstract(StringBuilder formula) {
+        String firstOperation = "+";
+        String secondOperation = "-";
+        int indOfFirstOperation;
+        int indOfSecondOperation;
+
+        // while first or second operation still exists
+        while ((indOfFirstOperation = formula.indexOf(firstOperation)) > -1 |
+                (indOfSecondOperation = formula.indexOf(secondOperation)) > -1) {
+            if (indOfSecondOperation == 0) { // first number is negative
+                indOfSecondOperation = formula.indexOf(secondOperation, 1);
+                String operation;
+                int operationInd;
+
+                // determine which operation goes first
+                if (indOfFirstOperation > -1 && indOfSecondOperation > -1) { // if both operations are present
+                    operation = indOfFirstOperation < indOfSecondOperation ? firstOperation : secondOperation;
+                    operationInd = Math.min(indOfFirstOperation, indOfSecondOperation);
+                } else { // if only one of 2 operations present
+                    if (indOfFirstOperation > -1) {
+                        operation = firstOperation;
+                        operationInd = indOfFirstOperation;
+                    } else { // only second operation is present
+                        operation = secondOperation;
+                        operationInd = indOfSecondOperation;
+                    }
+                }
+
+                int indOperationBefore = findOperationIndex(formula.toString(), operationInd - 1, false);
+                int nextOperationInd = findOperationIndex(formula.toString(), operationInd + 1, true);
+                int indOfDigitBeforeCurrentOperation = indOperationBefore + 1;
+
+                // if this is the last operation in the formula
+                if (nextOperationInd == -1) nextOperationInd = formula.length();
+
+                String expression = formula.substring(indOfDigitBeforeCurrentOperation, nextOperationInd);
+                double result = doNegArithmetic(expression, operation);
+
+                formula.replace(indOfDigitBeforeCurrentOperation, nextOperationInd, String.valueOf(result));
+            } else { // first number is positive
+                String operation;
+                int operationInd;
+
+                // determine which operation goes first
+                if (indOfFirstOperation > -1 && indOfSecondOperation > -1) { // if both operations are present
+                    operation = indOfFirstOperation < indOfSecondOperation ? firstOperation : secondOperation;
+                    operationInd = Math.min(indOfFirstOperation, indOfSecondOperation);
+                } else { // if only one of 2 operations present
+                    if (indOfFirstOperation > -1) {
+                        operation = firstOperation;
+                        operationInd = indOfFirstOperation;
+                    } else { // only second operation is present
+                        operation = secondOperation;
+                        operationInd = indOfSecondOperation;
+                    }
+                }
+
+                int indOperationBefore = findOperationIndex(formula.toString(), operationInd - 1, false);
+                int nextOperationInd = findOperationIndex(formula.toString(), operationInd + 1, true);
+                int indOfDigitBeforeCurrentOperation = indOperationBefore + 1;
+
+                // if this is the last operation in the formula
+                if (nextOperationInd == -1) nextOperationInd = formula.length();
+
+                String expression = formula.substring(indOfDigitBeforeCurrentOperation, nextOperationInd);
+                double result = doArithmetic(expression, operation);
+
+                formula.replace(indOfDigitBeforeCurrentOperation, nextOperationInd, String.valueOf(result));
+            }
+        }
+    }
+
     private static double doArithmetic(String formula, String operation) {
         int operationIndex = formula.indexOf(operation);
+        double res = -1;
+
+        double firstNum = Double.parseDouble(formula.substring(0, operationIndex));
+        double secondNum = Double.parseDouble(formula.substring(operationIndex + 1));
+
+        switch (operation) {
+            case "+" -> res = firstNum + secondNum;
+            case "-" -> res = firstNum - secondNum;
+            case "*" -> res = firstNum * secondNum;
+            case "/" -> res = firstNum / secondNum;
+            case "^" -> res = Math.pow(firstNum, secondNum);
+            default -> System.out.println("unknown operation");
+        }
+
+        return res;
+    }
+
+    private static double doNegArithmetic(String formula, String operation) {
+        int operationIndex = formula.indexOf(operation, 1);
         double res = -1;
 
         double firstNum = Double.parseDouble(formula.substring(0, operationIndex));
@@ -122,7 +227,7 @@ public class Assignment10Part1 {
             }
             return -1;
         } else {
-            for (int i = startInd; i > 0; i--) {
+            for (int i = startInd; i >= 0; i--) {
                 for (String operaion : prioritizedOperations) {
                     if (formula.charAt(i) == operaion.charAt(0)) return i; // index of operation
                 }
@@ -142,3 +247,5 @@ public class Assignment10Part1 {
 // "(1 + 3) ^ 2"
 // "1 + 3 * 2"
 // "1 + a * 2"
+// "5-10 + 3^2" fails
+// "-10 + 3"
