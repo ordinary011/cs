48308524c2a65e47a3f974bc614af7cfdd7510bd
zondagor <debryniuk@gmail.com>
2020-10-03 22:37:16 +0300

    refactored a bit

--- eleven/rpn/Assignment11Part1.java
+++ eleven/rpn/Assignment11Part1.java
@@ -8,8 +8,7 @@ public class Assignment11Part1 {
 //            return;
 //        }
 
-
-//        args = new String[]{"-(1+23/4^5+(-67/(cos(8)^9+sin(tan(atan(log2(10)^11)/12)*13)+14-1516))^17-18+(-19^(-20))*(-21)+22^23+tan(24)-sqrt(25)-26+27^28/29-30)/31^a+sqrt(sqrt(625))", "a=32"}; // {"-5.00000"},
+//        args = new String[]{"2-a", "a=-2"}; // {"4.00000"},
 //        System.out.println(new Calculator().runCalc(args));
 
         new Tester().runTests(); // todo uncomment for tests
--- eleven/rpn/Calculator.java
+++ eleven/rpn/Calculator.java
@@ -5,8 +5,11 @@ import java.util.Stack;
 
 public class Calculator {
 
+    // contains numbers as Strings. Can contain "-33.55555"
     private final Stack<String> nums = new Stack<>();
+    // contains operators like ^ * / + - (
     private final Stack<String> operators = new Stack<>();
+    // contains variables <a, -555.333>
     private final HashMap<String, String> vars = new HashMap<>();
 
     /**
@@ -18,33 +21,6 @@ public class Calculator {
             args[i] = args[i].replaceAll(" ", "");
             args[i] = args[i].replaceAll(",", ".");
         }
-    }
-
-    public String runCalc(String[] args) {
-        formatArgs(args);
-        String formula = args[0];
-
-        try {
-            parseVars(args);
-
-            parseFormula(formula);
-
-            // calculate the rest of the stack
-            String n2 = nums.pop();
-            while (!operators.empty()) {
-                String n1 = nums.pop();
-                String operation = operators.pop();
-
-                n2 = calcOperation(n1, n2, operation);
-            }
-
-            return String.format("%.5f", Double.parseDouble(n2));
-        } catch (Exception e) {
-            System.out.println("Input mistake. Please check your formula and variables");
-//            e.printStackTrace();
-        }
-
-        return null;
     }
 
     private void parseVars(String[] args) {
@@ -60,78 +36,63 @@ public class Calculator {
         }
     }
 
-    private void parseFormula(String formula) throws Exception {
-        int numInd = 0; // start index of a number before operation
-        char ch = formula.charAt(0);
-        if (ch == '(') {
-            operators.push("(");
-            numInd++; // i points to the "(". next num will have an index of i + 1;
+    public String runCalc(String[] args) {
+        formatArgs(args);
+        String formula = args[0];
+
+        boolean setSignOfRes = formula.startsWith("-("); // e.g. -(3-6)
+
+        try {
+            parseVars(args);
+            parseFormula(formula);
+
+            String res = calcRestOfStack();
+
+            if (setSignOfRes) {
+                if (res.charAt(0) == '-') res = res.substring(1); // e.g. -(3-7), res=-4 -> -(-4) -> 4
+                else res = "-" + res; // e. g. -(33+53), res=86 -> -(86) -> -86
+            }
+
+            return String.format("%.5f", Double.parseDouble(res));
+        } catch (Exception e) {
+            System.out.println("Input mistake. Please check your formula and variables");
         }
 
-        for (int i = 1; i < formula.length(); i++) {
-            ch = formula.charAt(i);
+        return null;
+    }
 
+    private String calcRestOfStack() {
+        String n2 = nums.pop();
+        while (!operators.empty()) {
+            String n1 = nums.pop();
+            String operation = operators.pop();
+
+            n2 = calcOperation(n1, n2, operation);
+        }
+
+        return n2;
+    }
+
+    private void parseFormula(String formula) throws Exception {
+        int numInd = ifParenthesesAtStart(formula.charAt(0));
+
+        for (int i = 1; i < formula.length(); i++) {
+            char ch = formula.charAt(i);
             if (isOp(ch)) {
                 if (isOp(formula.charAt(i - 1))) continue; // e.g. 10*-3. Here "i" points to "-" after "*"
-
-                pushToNumsStack(formula, numInd, i);
-
-                String formulaOp = formula.substring(i, i + 1);
-                if (!operators.empty()) {
-                    int formOpPriority = getPriority(formulaOp);
-                    String topOfStackOp = operators.peek();
-                    int topOfStackOpPriority = getPriority(topOfStackOp);
-
-                    while (formOpPriority <= topOfStackOpPriority) {
-                        String n2 = nums.pop();
-                        String n1 = nums.pop();
-                        operators.pop();
-
-                        String res = calcOperation(n1, n2, topOfStackOp);
-                        nums.push(res);
-
-                        if (!operators.empty()) {
-                            topOfStackOp = operators.peek();
-                            topOfStackOpPriority = getPriority(topOfStackOp);
-                        } else break;
-                    }
-                }
-                operators.push(formulaOp);
-
-                numInd = i + 1; // i points to the сurrent operator e.g. "*". next num will have an index of i + 1;
+                numInd = whenChIsOp(formula, numInd, i);
             } else if (ch == '(') {
-                // if func before
                 if (i > 2 && isFunc(formula.substring(numInd, i))) { // parentheses after function e.g. sin(3+3)
-                    int chNumTillClosing = findChNumTillClosing(formula.substring(i));
-
-                    parseFormula(formula.substring(i, (i + chNumTillClosing + 1)));
-                    // after parseFormula at the top of the stack will be res of calculations withing brackets
-                    String res = nums.pop();
-                    res = calcFunc(formula.substring(numInd, i), res);
-                    nums.push(res);
-
+                    int chNumTillClosing = parseFunction(formula, numInd, i);
                     i += chNumTillClosing;
                     numInd = i + 1;
-                } else { // regular parentheses
+                } else { // regular parentheses without a function e.g. (3+3)
                     operators.push("(");
                     numInd = i + 1; // i points to the "(". next num will have an index of i + 1;
-                    i++; // skip ch after "(" because numInd will point to that number anyway
+                    i++; // skip ch after "(" because numInd will point to that number or to that sign anyway
                 }
             } else if (ch == ')') {
-                pushToNumsStack(formula, numInd, i);
-
-                String operation = operators.pop();
-                String res = "";
-                while (!operation.equals("(")) {
-                    String n2 = nums.pop();
-                    String n1 = nums.pop();
-
-                    res = calcOperation(n1, n2, operation);
-                    nums.push(res);
-
-                    operation = operators.pop();
-                }
-                numInd = i + 1; // i points to the ")". next num will have an index of i + 1;
+                numInd = calcTillOpeningParantheses(formula, numInd, i);
             }
         }
 
@@ -139,6 +100,71 @@ public class Calculator {
         pushToNumsStack(formula, numInd, formula.length());
     }
 
+    private int calcTillOpeningParantheses(String formula, int numInd, int i) throws Exception {
+        pushToNumsStack(formula, numInd, i); // push last num before closing parantheses
+
+        String operation = operators.pop();
+        while (!operation.equals("(")) {
+            operation = doStackIteration(operation);
+        }
+
+        return i + 1; // i points to the ")". next num will have an index of i + 1;
+    }
+
+    private int parseFunction(String formula, int numInd, int i) throws Exception {
+        int chNumTillClosing = findChNumTillClosing(formula.substring(i));
+
+        parseFormula(formula.substring(i, (i + chNumTillClosing + 1)));
+        // after parseFormula at the top of the stack will find the result of calculations within brackets
+        String res = nums.pop(); // e.g. cos(3+3). 6 is at top of the stack
+        res = calcFunc(formula.substring(numInd, i), res);
+        nums.push(res);
+
+        return chNumTillClosing;
+    }
+
+    private String doStackIteration(String operation) {
+        String n2 = nums.pop();
+        String n1 = nums.pop();
+
+        String res = calcOperation(n1, n2, operation);
+        nums.push(res);
+
+        return operators.pop();
+    }
+
+    private int whenChIsOp(String formula, int numInd, int i) throws Exception {
+        pushToNumsStack(formula, numInd, i);
+
+        String formulaOp = formula.substring(i, i + 1);
+        if (!operators.empty()) {
+            int formOpPriority = getPriority(formulaOp);
+            String topOfStackOp = operators.peek();
+            int topOfStackOpPriority = getPriority(topOfStackOp);
+
+            while (formOpPriority <= topOfStackOpPriority) {
+                doStackIteration(topOfStackOp);
+
+                if (!operators.empty()) {
+                    topOfStackOp = operators.peek();
+                    topOfStackOpPriority = getPriority(topOfStackOp);
+                } else break; // if stack is empty we break
+            }
+        }
+        operators.push(formulaOp);
+
+        numInd = i + 1; // i points to the сurrent operator e.g. "*". next num will have an index of i + 1;
+        return numInd;
+    }
+
+    private int ifParenthesesAtStart(char firstChInFormula) {
+        if (firstChInFormula == '(') {
+            operators.push("(");
+            return 1; // numInd points to the "(". num after "(" will have an index of 1;
+        }
+
+        return 0; // if no parentheses at the beginning numInd is to be set to 0
+    }
 
 
     private void pushToNumsStack(String formula, int numInd, int endIndOfNum) throws Exception { // num can be: 6 || -6 || a || -a
@@ -151,12 +177,11 @@ public class Calculator {
                 if (varValue != null) {
                     if (varValue.charAt(0) == '-' && // if args var is negative e.g. a=-2
                             num.charAt(0) == '-' // if var in formula is negative
-                    ) // e.g. 5-a || 5^-a || -a+5
-                    {
+                    ) { // cases to consider 5-a || 5^-a || -a+5
                         num = varValue.substring(1); // var -2 -> 2
-                    } else if (num.charAt(0) == '-') { // 10/-a
+                    } else if (num.charAt(0) == '-') { // if var in formula is negative 10/-a
                         num = "-" + varValue;
-                    } else {
+                    } else { // sign of var is already correct
                         num = varValue;
                     }
                 } else {
@@ -169,7 +194,7 @@ public class Calculator {
         }
     }
 
-    private String findVar(String varName) { // a || -a
+    private String findVar(String varName) { // varName can be: a || -a
         if (varName.charAt(0) == '-') varName = varName.substring(1);
 
         return vars.get(varName);
@@ -187,17 +212,12 @@ public class Calculator {
         return str.matches(".?[a-zA-Z]+");
     }
 
-    private boolean isDigit(char ch) {
-        return Character.isDigit(ch);
-    }
-
     /**
      * The following method
      */
     private int getPriority(String operation) {
         switch (operation) {
             case "(":
-//            case ")":
                 return 0;
             case "+":
             case "-":
