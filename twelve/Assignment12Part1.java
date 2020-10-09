fc78a9f57ff726997aa72b3777fa301629eedc60
zondagor <debryniuk@gmail.com>
2020-10-09 23:26:10 +0300

    works for png

--- twelve/Assignment12Part1.java
+++ twelve/Assignment12Part1.java
@@ -5,6 +5,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class Assignment12Part1 {
 
@@ -12,25 +13,28 @@ public class Assignment12Part1 {
     private static int[][] pixels;
     private static int w;
     private static int h;
-    private static int BACKGROUND_COLOR = -1;
-//    private static int BACKGROUND_COLOR = -16184566;
+    private static int BG_COLOR;
 
     public static void main(String[] args) {
 //        if (args.length == 0) go("test.jpg");
 //        else go(args[0]);
 
-        go("testdrive.jp");
+//        go("testdrive.jpg");
+        go("skater.jpg");
+//        go("skater.png");
+//        go("testColors_new.png");
+//        go("test41.png");
     }
 
     private static void go(String imgName) {
         try {
             BufferedImage img = ImageIO.read(new File(imgName));
 
-            getPixValues(img);
+            createArrOfPixs(img);
 
             findBGColor();
 
-            countObjects();
+//            countObjects();
 
 
 //            isRelated(124, 122);
@@ -42,7 +46,7 @@ public class Assignment12Part1 {
 //            for (int i = 0; i < pixels.length; i++) {
 //                for (int j = 0; j < pixels[0].length; j++) {
 ////                    System.out.println(w * i + j);
-//                    System.out.print(pixels[124][j] + " ");
+//                    System.out.println(pixels[40][j] + " ");
 //                }
 //                System.out.println();
 //            }
@@ -52,28 +56,30 @@ public class Assignment12Part1 {
     }
 
     private static void findBGColor() {
+        // <pixVal, pixCountOfCurrentPixValue>
         HashMap<Integer, Integer> pixPopularity = new HashMap<>();
 
-        for (int i = 0; i < pixels.length; i++) {
-            for (int j = 0; j < pixels[0].length; j++) {
-                int pixVal = pixels[i][j];
-                if (!pixPopularity.containsKey(pixVal)) {
-                    pixPopularity.put(pixVal, 0);
-                } else {
-//                    pixPopularity.put(pixVal, pixPopularity.get(pixVal) + 1); // increment popularity
-                    pixPopularity.compute(pixVal, (k, v) -> v + 1); // increment popularity
-                }
+        for (int[] pixRow : pixels) {
+            for (int pixVal : pixRow) {
+                if (!pixPopularity.containsKey(pixVal)) pixPopularity.put(pixVal, 0);
+                else pixPopularity.compute(pixVal, (k, v) -> v + 1); // increment popularity
             }
         }
 
-//        int res = 0;
-//        int pop = 0;
-//        for (Integer pixVal : pixPopularity.) {
-//            if ()
-//        }
+        int mostPopularVal = 0;
+        int biggestPixCount = 0;
+        for (Map.Entry<Integer, Integer> entry : pixPopularity.entrySet()) {
+            int pixCountOfPixVal = entry.getValue();
+            if (pixCountOfPixVal > biggestPixCount) {
+                mostPopularVal = entry.getKey();
+                biggestPixCount = pixCountOfPixVal;
+            }
+        }
+
+        BG_COLOR = mostPopularVal;
     }
 
-    private static void getPixValues(BufferedImage img) {
+    private static void createArrOfPixs(BufferedImage img) {
         w = img.getWidth();
         h = img.getHeight();
 
@@ -89,10 +95,9 @@ public class Assignment12Part1 {
 
         for (int i = 0; i < pixels.length; i++) {
             for (int j = 0; j < pixels[0].length; j++) {
-                if (pixels[i][j] != BACKGROUND_COLOR) {
-                    if (!visitedPixs.contains(w * i + j)) { // if pix was not visited
-                        if (isRelated(i, j)) objCount++;
-                    }
+                if (pixels[i][j] != BG_COLOR && !visitedPixs.contains(w * i + j)) { // if pix was not visited
+                    if (isRelated(i, j)) // if not single pix
+                        objCount++;
                 }
             }
         }
@@ -107,56 +112,56 @@ public class Assignment12Part1 {
     }
 
     private static boolean isRelated(int i, int j) {
-        boolean isRelated = false;
+        boolean isNotSinglePix = false;
 //        System.out.println(w * i + j);
         visitedPixs.add(w * i + j);
 
-        if (j < w - 1 && pixels[i][j + 1] != BACKGROUND_COLOR) { // if right pixel is black
+        if (j < w - 1 && pixels[i][j + 1] != BG_COLOR) { // if right pixel is black
             check(i, j + 1);
-            isRelated = true;
+            isNotSinglePix = true;
         }
 
-        if (i < h - 1 && j < w - 1 && pixels[i + 1][j + 1] != BACKGROUND_COLOR) { // if below right pix is black
+        if (i < h - 1 && j < w - 1 && pixels[i + 1][j + 1] != BG_COLOR) { // if below right pix is black
             check(i + 1, j + 1);
-            isRelated = true;
+            isNotSinglePix = true;
         }
 
-        if (i < h - 1 && pixels[i + 1][j] != BACKGROUND_COLOR) { // if pix below is black
+        if (i < h - 1 && pixels[i + 1][j] != BG_COLOR) { // if pix below is black
             check(i + 1, j);
-            isRelated = true;
+            isNotSinglePix = true;
         }
 
-        if (i < h - 1 && j > 0 && pixels[i + 1][j - 1] != BACKGROUND_COLOR) { // if below left pix is black
+        if (i < h - 1 && j > 0 && pixels[i + 1][j - 1] != BG_COLOR) { // if below left pix is black
             check(i + 1, j - 1);
-            isRelated = true;
+            isNotSinglePix = true;
         }
 
-        if (j > 0 && pixels[i][j - 1] != BACKGROUND_COLOR) { // if left pixel is black
+        if (j > 0 && pixels[i][j - 1] != BG_COLOR) { // if left pixel is black
             check(i, j - 1);
-            isRelated = true;
+            isNotSinglePix = true;
         }
 
-        if (i > 0 && j > 0 && pixels[i - 1][j - 1] != BACKGROUND_COLOR) { // if above left pix is black
+        if (i > 0 && j > 0 && pixels[i - 1][j - 1] != BG_COLOR) { // if above left pix is black
             check(i - 1, j - 1);
-            isRelated = true;
+            isNotSinglePix = true;
         }
 
-        if (i > 0 && pixels[i - 1][j] != BACKGROUND_COLOR) { // if pix above is black
+        if (i > 0 && pixels[i - 1][j] != BG_COLOR) { // if pix above is black
             check(i - 1, j);
-            isRelated = true;
+            isNotSinglePix = true;
         }
 
-        if (i > 0 && j < w - 1 && pixels[i - 1][j + 1] != BACKGROUND_COLOR) { // if above right pix is black
+        if (i > 0 && j < w - 1 && pixels[i - 1][j + 1] != BG_COLOR) { // if above right pix is black
             check(i - 1, j + 1);
-            isRelated = true;
+            isNotSinglePix = true;
         }
 
-        return isRelated;
+        return isNotSinglePix;
     }
 
 
 }
 
 // testColors_new.png:
-// i: 124, j: 122
-// 26906 pix value = -65540
+// i: 124, j: 122 subNum: 26906
+// pix value = -65540
